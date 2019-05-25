47206684a726decc3c85e1bc6a67ed9bb48721d6
Birger Moëll <birger.moell@gmail.com>
2019-05-25 13:12:25 +0200

    Added chat function template

---
+++ src/chat.rs
@@ -0,0 +1,17 @@
+extern crate reqwest;
+use http::{Request, Response};
+use serde_json::{Result, Value};
+use rand::Rng;
+use std::process::Command;
+use std::str;
+
+pub fn sendChatMessage(outputMessage: &str) {
+
+}
+
+pub fn getChatMessage(inputMessage: String) {
+
+}
+
+
+
